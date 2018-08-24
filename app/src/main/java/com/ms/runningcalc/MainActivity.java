a70200ed12874c2a29f2bda1fd6b9a2fd282b631
Florke07 <maciek.socha1@gmail.com>
2018-08-24 19:17:23 +0200

    Initial commit

---
+++ app/src/main/java/com/ms/runningcalc/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ms.runningcalc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.ms.runningcalc.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void openTimeDistance(View view) {
+        Intent intent = new Intent(this, TimeDistance.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void openPaceDistnace(View view) {
+        Intent intent = new Intent(this, PaceDistance.class);
+        startActivity(intent);
+    }
+}
