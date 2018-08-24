4ed8a8c9609998dac83725c07ca0ebfc16eb1087
Florke07 <maciek.socha1@gmail.com>
2018-08-24 21:15:41 +0200

    Time nad Distance activity work, but need a lot of changes e.x. make it user-friendly

--- app/src/main/java/com/ms/runningcalc/PaceDistance.java
+++ app/src/main/java/com/ms/runningcalc/PaceDistance.java
@@ -2,6 +2,10 @@ package com.ms.runningcalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.ms.runningcalc.units.Distance;
 
 public class PaceDistance extends AppCompatActivity {
 
@@ -10,4 +14,16 @@ public class PaceDistance extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pace_distance);
     }
+
+    public void calc(View view) {
+        EditText distanceText = (EditText) findViewById(R.id.editText2);
+        EditText paceText = (EditText) findViewById(R.id.editText3);
+
+        float dst = Float.valueOf(distanceText.getText().toString());
+        Distance distance = new Distance(dst);
+
+        String tm = paceText.getText().toString();
+
+    }
+
 }
--- app/src/main/java/com/ms/runningcalc/TimeDistance.java
+++ app/src/main/java/com/ms/runningcalc/TimeDistance.java
@@ -3,7 +3,15 @@ package com.ms.runningcalc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.ms.runningcalc.logic.Calculations;
+import com.ms.runningcalc.units.Distance;
+import com.ms.runningcalc.units.Pace;
+import com.ms.runningcalc.units.Speed;
+import com.ms.runningcalc.units.Time;
 
 public class TimeDistance extends AppCompatActivity {
 
@@ -19,5 +27,30 @@ public class TimeDistance extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         //TextView textView = findViewById(R.id.textView);
         //textView.setText(message);
+    }
+
+    public void calc(View view) {
+        EditText distanceText = (EditText) findViewById(R.id.editText2);
+        EditText timeText = (EditText) findViewById(R.id.editText3);
+
+        float dst = Float.valueOf(distanceText.getText().toString());
+        Distance distance = new Distance(dst);
+
+        String tm = timeText.getText().toString();
+        //todo I know, this is terrible
+        int tmp1 = (Character.getNumericValue(tm.charAt(0))*10) + Character.getNumericValue(tm.charAt(1));
+        int tmp2 = (Character.getNumericValue(tm.charAt(3))*10)+Character.getNumericValue(tm.charAt(4));
+        int tmp3 = (Character.getNumericValue(tm.charAt(6))*10)+Character.getNumericValue(tm.charAt(7));
+        Time time = new Time(tmp1, tmp2, tmp3);
+
+        Pace calcedPace = Calculations.calcPace(time, distance);
+        Speed clalcedSpeed = Calculations.calcSpeed(time, distance);
+
+        EditText paceText = (EditText) findViewById(R.id.editText4);
+        paceText.setText(calcedPace.getPace());
+
+        EditText speedText = (EditText) findViewById(R.id.editText5);
+        speedText.setText(String.valueOf(clalcedSpeed.getKmh()));
+
     }
 }
